5a7774269cb5a1fba8b7ac1a99a757ae2e80f9c5
tymey <tyler.meyer.a@gmail.com>
2025-01-07 14:47:03 -0600

    Refactor: App into a BrowserRouter from react-router

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import WorkoutSearch from './workouts/WorkoutSearch.jsx';
-import WorkoutList from './workouts/WorkoutList.jsx';
-import Nutrition from './nutrition/Nutrition.jsx';
-import Navigation from './navigation/Navigation.jsx';
-import Routines from './workouts/Routines.jsx';
-import Dashboard from './dashboard/Dashboard.jsx';
-import AccountPage from './navigation/AccountPage.jsx';
 import axios from 'axios';
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+} from 'react-router-dom';
+
+// Views:
+import Account from './views/Account.jsx';
+import Home from './views/Home.jsx';
+import Exercises from './views/Exercises.jsx';
+import ExercisesSearch from './views/ExercisesSearch.jsx';
+import Login from './views/Login.jsx';
+import MealPlans from './views/MealPlans.jsx';
+import Pantry from './views/Pantry.jsx';
+import Workouts from './views/Workouts.jsx';
+
 import { ThemeProvider, CssBaseline, Switch, IconButton } from '@mui/material';
-import { lightTheme, darkTheme } from './styles';
+import { lightTheme, darkTheme } from './components/styles.js';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
-import CurrentMealPlans from './nutrition/CurrentMealPlans.jsx';
+
 
 
 export default function App() {
   //theme toggle light/dark
   const [darkMode, setDarkMode] = useState(false);
-	const [view, setView] = useState('Dashboard');
 	const [user, setUser] = useState(null);
 	const [routines, setRoutines] = useState(null);
 
@@ -38,105 +46,6 @@ export default function App() {
 			})
 	}
 
-	function updateView(e) {
-
-		switch (e.target.name){
-			case 'Logout':
-				axios.post('/user/logout')
-					.then((response) => {
-						if (response.status === 200){
-							window.location.href = "/";
-						}
-					})
-					.catch((error) => {
-						console.error('Error on POST logout in main.', error)
-					})
-				break;
-			case 'Workouts':
-				axios.get('/user/workouts')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET workouts view in main.', error)
-					})
-				break;
-			case 'Workouts-Search':
-				axios.get('/user/workouts/search')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET workouts search view in main.', error)
-					})
-					break;
-			case 'Nutrition':
-				axios.get('/user/nutrition')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET nutrition view in main.', error)
-					})
-					break;
-			case 'Routines':
-				axios.get('/user/routines')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET routines view in main.', error)
-					})
-				break;
-			case 'Delete Account':
-				const warningMessage = `Are you sure that you would like to delete your account?\nThis action is irreversible.`
-				if (window.confirm(warningMessage)){
-					axios.delete('/user/delete')
-						.then((response) => {
-							if (response.status === 200){
-								window.location.href = "/";
-							}
-						})
-						.catch((error) => {
-							console.error('Error on DELETE user in main.', error)
-						})
-				} else {
-					return;
-				}
-
-			case 'Dashboard':
-				axios.get('/user/dashboard')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET nutrition view in main.', error)
-					})
-					break;
-			
-			case 'Account Page':
-				axios.get('/user/account/')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET account page view in main.', error)
-					})
-					break;
-			case 'Meal Plans':
-				axios.get('/user/nutrition/meals')
-					.then((response) => {
-						setView(response.data.view);
-					})
-					.catch((error) => {
-						console.error('Error on GET account page view in main.', error)
-					})
-					break;
-			default:
-				console.error('Client error for update view in main.')
-		}
-	}
-
   const handleThemeChange = () => {
     setDarkMode((prevMode) => !prevMode);
   };
@@ -146,134 +55,18 @@ export default function App() {
 		setTimeout( () => {fetchUser()}, 0)
 	}, [])
 
-	switch(view){
-		case 'Workouts':
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-          <CssBaseline />
-          <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-				<Navigation updateView={updateView}/>
-				<br></br>
-				{user ?
-					<div>
-					<WorkoutList user={user} fetchUser={fetchUser} workouts={user.workouts}/>
-					</div>
-					:
-					<div>
-						<h1 style={{textAlign:"center"}}>401</h1>
-						<h2 style={{textAlign:"center"}}>Unauthorized; please re-attempt login.</h2>
-					</div>
-					}
-				</div>
-        </ThemeProvider>
-			)
-		case 'Workouts-Search':
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-				<Navigation updateView={updateView}/>
-				<br></br>
-					<div>
-					<WorkoutSearch user={user} fetchUser={fetchUser}/>
-					</div>
-				</div>
-        </ThemeProvider>
-			)
-		case 'Nutrition':
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-					<Navigation updateView={updateView}/>
-
-					<br></br>
-					<div><Nutrition state={darkMode} fetchUser={fetchUser} user={user} routines={routines}/></div>
-
-				</div>
-        </ThemeProvider>
-			)
-			case 'Routines':
-				return (
-					<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-						<CssBaseline />
-						<IconButton onClick={handleThemeChange} color="inherit">
-						<Brightness4Icon />
-					</IconButton>
-					<div id="root-app">
-					<Navigation updateView={updateView}/>
-					<br></br>
-					{user ?
-						<div>
-						<Routines user={user} fetchUser={fetchUser} routines={routines} workouts={user.workouts}/>
-						</div>
-						:
-						<div>
-							<h1 style={{textAlign:"center"}}>401</h1>
-							<h2 style={{textAlign:"center"}}>Unauthorized; please re-attempt login.</h2>
-						</div>
-						}
-					</div>
-					</ThemeProvider>
-				)
-		case 'Account Page':
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-					<Navigation updateView={updateView}/>
-					<br></br>
-					<div><AccountPage state={darkMode} user={user} updateView={updateView} fetchUser={fetchUser}/></div>
-				</div>
-        </ThemeProvider>
-			)
-		case 'Meal Plans':
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-					<Navigation updateView={updateView}/>
-					<br></br>
-					<div><CurrentMealPlans state={darkMode} user={user} updateView={updateView} fetchUser={fetchUser}/></div>
-				</div>
-        </ThemeProvider>
-			)
-		default:
-			return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <CssBaseline />
-        <IconButton onClick={handleThemeChange} color="inherit">
-          <Brightness4Icon />
-        </IconButton>
-				<div id="root-app">
-					<Navigation updateView={updateView}/>
-					{user ?
-					<div>
-						<Dashboard user={user}/>
-					</div>
-					:
-					<div>
-						<h1 style={{textAlign:"center"}}>401</h1>
-						<h2 style={{textAlign:"center"}}>Unauthorized; please re-attempt login.</h2>
-					</div>
-					}
-				</div>
-        </ThemeProvider>
-			)
-	}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="home" element={<Home />} />
+				<Route path="account" element={<Account />} />
+				<Route path="exercises" element={<Exercises />} />
+				<Route path="exercises-search" element={<ExercisesSearch />} />
+				<Route path="meal-plans" element={<MealPlans />} />
+				<Route path="pantry" element={<Pantry />} />
+				<Route path="workouts" element={<Workouts />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
