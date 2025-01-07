50410d62aec508652fbbbbbd5f51e1b56ce882f6
tymey <tyler.meyer.a@gmail.com>
2025-01-07 14:04:44 -0600

    Create: Nutrtion view

---
+++ client/views/Nutrition.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Nutrition() {
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
+      <IconButton onClick={handleThemeChange} color="inherit">
+        <Brightness4Icon />
+      </IconButton>
+			<div id="root-app">
+				<Navigation updateView={updateView}/>
+
+				<br></br>
+				<div><Nutrition state={darkMode} fetchUser={fetchUser} user={user} routines={routines}/></div>
+
+			</div>
+    </ThemeProvider>
+  );
+}
+
+export default Nutrition;
