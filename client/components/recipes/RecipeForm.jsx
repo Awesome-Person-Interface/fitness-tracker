2915deb89549ef17cf0e5e1048d97bdb3184c5d9
evan.loria4 <evan.loria4@gmail.com>
2025-01-07 19:38:50 -0600

    Set the fullWidth and maxWidth props on dailog box

--- client/components/recipes/RecipeForm.jsx
+++ client/components/recipes/RecipeForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import {
   FormControl,
@@ -7,17 +7,34 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Typography,
+  TextField,
 } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 function RecipesForm({ makingRecipe, setMakingRecipe }) {
+  const [formValues, setFormValues] = useState({ name: '' });
   return (
-    <FormControl>
-      <Dialog open={makingRecipe}>
+      <Dialog
+        open={makingRecipe}
+        maxWidth='lg'
+        fullWidth
+        >
         <DialogTitle>
           Build a Recipe
         </DialogTitle>
+        <DialogContent>
+          <FormControl>
+            <Grid container spacing={12}>
+              <Grid>
+            <TextField
+             label="Recipe Name"
+            />
+              </Grid>
+            </Grid>
+          </FormControl>
+        </DialogContent>
       </Dialog>
-    </FormControl>
   )
 }
 
