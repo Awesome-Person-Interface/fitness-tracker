d53e85414af14c34f8fe3c0564891af48868e4fa
tymey <tyler.meyer.a@gmail.com>
2025-01-08 12:29:16 -0600

    Update: Fix dialog size, descructure dateSlot prop

--- client/components/calendar/CreateEventDialog.jsx
+++ client/components/calendar/CreateEventDialog.jsx
@@ -4,16 +4,48 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  DialogContentText,
   DialogActions,
+  Button,
 } from '@mui/material';
 
-function CreateEventDialog({ openDialog, handleCloseDialog }) {
+function CreateEventDialog({ dateSlot, handleCloseDialog }) {
+  /*
+    start: The date at the start of the dateSlot selected
+    end: The date at the end of the dateSlot selected
+      - (Exclusive) Not included in the range shown on the calendar.
+  */
+  const { start, end } = dateSlot;
   return (
     <Dialog
-      open={openDialog}
+      open={!!dateSlot.start}
       onClose={handleCloseDialog}
+      fullWidth={true}
+      maxWidth="lg"
     >
       <DialogTitle>Create Event</DialogTitle>
+      {
+        dateSlot.start
+          ? (
+            <DialogContent>
+              <DialogContentText>{`Start: ${start}`}</DialogContentText>
+              <DialogContentText>{`End: ${end}`}</DialogContentText>
+            </DialogContent>
+          )
+          : (
+            <DialogContent>
+              <DialogContentText>Closing...</DialogContentText>
+            </DialogContent>
+          )
+      }
+
+      <DialogActions>
+        <Button
+          onClick={handleCloseDialog}
+        >
+          Close
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
