22c47ab12ac56b78181b5b8cad5af925e1c73b6a
evan.loria4 <evan.loria4@gmail.com>
2025-01-07 19:54:54 -0600

    Initialize IngredientInput component

---
+++ client/components/recipes/IngredientInput.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+
+function IngredientInput() {
+  return (
+  <InputLabel>
+    <Select>
+      <MenuItem>Option1</MenuItem>
+      <MenuItem>Option2</MenuItem>
+      <MenuItem>Option3</MenuItem>
+    </Select>
+  </InputLabel>
+  )
+}
