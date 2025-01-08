3265f6c5d58b57022b49a32a9f35d12b6ba562d7
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 12:38:53 -0600

    Render input fields with select for ingredients

--- client/components/recipes/IngredientInput.jsx
+++ client/components/recipes/IngredientInput.jsx
@@ -1,20 +1,43 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import {
+  Button,
   InputLabel,
   Select,
   MenuItem,
+  TextField,
 } from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
+const measurements = ['tsp', 'tbsp', 'cup', 'oz', 'fl oz' ]
 function IngredientInput() {
+  // State value to hold the chosen measurement
+  const [measurement, setMeasurement] = useState('');
   return (
-  <InputLabel>
-    <Select>
-      <MenuItem>Option1</MenuItem>
-      <MenuItem>Option2</MenuItem>
-      <MenuItem>Option3</MenuItem>
-    </Select>
-  </InputLabel>
+  <Grid container spacing={1}>
+    <Grid>
+      <TextField label="Amount" />
+    </Grid>
+    <Grid>
+    <InputLabel>
+      <Select value={measurement}>
+        {measurements.map((measurement) => {
+          return (
+            <MenuItem
+              key={measurement}
+              value={measurement}
+              >
+                {measurement}
+            </MenuItem>
+          )
+        })}
+      </Select>
+    </InputLabel>
+    </Grid>
+    <Grid>
+      <TextField label="Ingredient" />
+    </Grid>
+  </Grid>
   )
 };
 
