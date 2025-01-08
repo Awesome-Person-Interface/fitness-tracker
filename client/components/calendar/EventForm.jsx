e1026db4a6a61b02dad353c56d620611d18e8399
tymey <tyler.meyer.a@gmail.com>
2025-01-08 17:29:29 -0600

    Add: TimePickers to choose start and end.

--- client/components/calendar/EventForm.jsx
+++ client/components/calendar/EventForm.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@mui/material/Grid2';
 import {
   Typography,
 } from '@mui/material';
+import {
+  LocalizationProvider,
+  TimePicker,
+} from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
-function EventForm({ update, create }) {
+function EventForm({ update, create, eventDetails }) {
+  const [start, setStart] = useState(dayjs(eventDetails.start));
+  const [end, setEnd] = useState(dayjs(eventDetails.end));
+  console.log('Start:', start);
+  console.log('End:', end);
   return (
     <Grid container spacing={2}>
       <Grid size={6}>
-        <Typography variant="subtitle2">Start:</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <TimePicker
+            label="Start Time"
+            value={start}
+            onChange={(newTime) => setStart(newTime)}
+          />
+        </LocalizationProvider>
       </Grid>
       <Grid size={6}>
-      <Typography variant="subtitle2">End:</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <TimePicker
+            label="End Time"
+            value={end}
+            onChange={(newTime) => setEnd(newTime)}
+          />
+        </LocalizationProvider>
       </Grid>
     </Grid>
   );
