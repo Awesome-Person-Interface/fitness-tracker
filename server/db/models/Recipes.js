d9209b84f2cf6828f0e689f80683ff01acb8035e
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 16:39:37 -0600

    Initialize Recipes model for the database

---
+++ server/db/models/Recipes.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+// Initialize the ingredients schema
+const IngredientSchema = new Schema({
+  name: {type: String, default: null },
+  serves: {type: String, default: null},
+  time: { type: String, default: null },
+});
+// Initialize the recipes schema
+  // Use the iIngredientSchema for the ingredients field in recipes
+  // Declaring an array of objects that follow the IngredientSchema
+const RecipeSchema = new Schema({
+  name: { type: String, },
+  serves: { type: String, },
+  time: { type: String, },
+  ingredients: [IngredientSchema],
+});
+// Create the recipes model using RecipeSchema
+const Recipes = mongoose.model('Recipe', RecipeSchema);
+// Export the recipes model
+export default Recipes;
