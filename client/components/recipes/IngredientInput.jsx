3e07b38aeea161e21ab1ea6fdaf73c4c0c827969
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 15:06:55 -0600

    Handle change for all form fields in state

--- client/components/recipes/IngredientInput.jsx
+++ client/components/recipes/IngredientInput.jsx
@@ -11,16 +11,29 @@ import Grid from '@mui/material/Grid2';
 
 const measurements = ['tsp', 'tbsp', 'cup', 'oz', 'fl oz' ]
 function IngredientInput({ value, index, formValues, setFormValues}) {
-  // Function to handle changes in ingredient fields
+  // Function to handle changes in ingredient and amount fields
   const handleIngredientChange = (element) => {
-    // Grab the value from the element
-    const { value } = element.target
+    // Grab the value and the id from the element
+      // Id represents the property of the ingredient object to be changed
+    const { value, id } = element.target
     // Make a copy of formValues
     const formCopy = { ...formValues };
     // On the formCopy ingredients property
-      // Change the name property on the ingredient object at the current index (index comes from state)
-    formCopy.ingredients[index].name = value;
+      // Change the property on the ingredient object at the current index (index comes from state)
+    formCopy.ingredients[index][id] = value;
     // Set the formValues in state to new value
+    setFormValues(formCopy);
+  }
+  // Function to handle the choice of unit in the select field
+  const handleUnitClick = (element) => {
+    console.log('Element: ', element);
+    console.log('Target: ', element.target);
+    console.log('Dataset: ', element.dataset);
+    // Grab the value from the element
+    const { value } = element.target.dataset;
+    console.log('Value: ', value);
+    const formCopy = { ...formValues };
+    formCopy.ingredients[index].unit = value;
     setFormValues(formCopy);
   }
   console.log('FormValues: ', formValues);
@@ -29,14 +42,19 @@ function IngredientInput({ value, index, formValues, setFormValues}) {
     <Grid>
       <TextField
         label="Amount"
+        id="amount"
+        onChange={handleIngredientChange}
         />
     </Grid>
     <Grid>
     <InputLabel>
-      <Select value={''}>
+      <Select
+        value={formValues.ingredients[index].unit}
+        >
         {measurements.map((measurement) => {
           return (
             <MenuItem
+              onClick={handleUnitClick}
               key={measurement}
               value={measurement}
               >
@@ -51,6 +69,7 @@ function IngredientInput({ value, index, formValues, setFormValues}) {
       <TextField
       label="Ingredient"
       onChange={handleIngredientChange}
+      id="name"
       value={value}
       />
     </Grid>
