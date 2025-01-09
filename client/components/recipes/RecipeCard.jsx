abc4d74638c038fc33eed854ebdaeb810e0a911b
evan.loria4 <evan.loria4@gmail.com>
2025-01-09 14:55:01 -0600

    Render dialog box for deleting a recipe

--- client/components/recipes/RecipeCard.jsx
+++ client/components/recipes/RecipeCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import {
   Button,
@@ -6,12 +6,23 @@ import {
   CardMedia,
   CardContent,
   CardActions,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Typography,
 } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import IngredientList from './IngredientList.jsx';
 
 function RecipeCard({ recipe }) {
+  // State vale for tracking if a recipe is being deleted
+  const [deletingRecipe, setDeletingRecipe] = useState(false);
+  // Function to delete the recipe from the database
+  const deleteRecipe = () => {
+
+  }
   return (
     <Grid
       size={4}
@@ -32,9 +43,30 @@ function RecipeCard({ recipe }) {
           <IngredientList ingredients={recipe.ingredients}/>
         </CardContent>
         <CardActions>
-          <Button>Delete</Button>
+          <Button
+            onClick={() => { setDeletingRecipe(true) }}
+          >
+            Delete
+          </Button>
         </CardActions>
       </Card>
+      <Dialog open={deletingRecipe}>
+        <DialogTitle>
+          {`Delete ${recipe.name}`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {`Are you sure you want to delete your ${recipe.name} recipe?`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => { setDeletingRecipe(false) }}
+          >
+            Cancel
+          </Button>
+          <Button>Delete</Button>
+        </DialogActions>
+      </Dialog>
     </Grid>
   )
 };
