6ef9e276593b93970c98d8950f9b76633bd0c019
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 23:31:25 -0600

    Render a card for each recipe in the database

--- client/components/recipes/RecipesGrid.jsx
+++ client/components/recipes/RecipesGrid.jsx
@@ -1,13 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Grid from '@mui/material/Grid2';
 import {
-
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
 } from '@mui/material'
 
 function RecipesGrid() {
+    // Hold the recipes in state
+    const [recipes, setRecipes] = useState([]);
+    // GET the recipes from the database
+    const getRecipes = () => {
+      console.log('Get recipes invoked');
+      // Make axios GET req to /user/recipes
+      axios.get('/user/recipes')
+        .then(({ data }) => {
+          setRecipes(data);
+      }).catch((err) => {
+          console.error('Error GETting the recipes from the server: ', err);
+      });
+        };
+        // When the component mounts, call getRecipes
+        useEffect(getRecipes, []);
   return (
-    <Grid container spacing={1}>
+    <Grid>
+      {recipes.map((recipe) => {
+        return (
+          <Card
+            sx={{ maxWidth: 345 }}
+            key={recipe._id}
+            >
+          <CardContent>
+            <Typography
+              variant="h6"
+            >
+              {recipe.name}
+            </Typography>
+          </CardContent>
+        </Card>
+        )
+      })}
     </Grid>
   )
-}
+};
+
+export default RecipesGrid;
