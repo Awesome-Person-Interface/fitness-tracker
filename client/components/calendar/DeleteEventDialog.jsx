452b0a38476af6c7d242742baaa025fa097b0bb9
tymey <tyler.meyer.a@gmail.com>
2025-01-09 15:39:52 -0600

    CREATE: DeleteEventDialog component to confirm delete

---
+++ client/components/calendar/DeleteEventDialog.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from '@mui/material';
+
+function DeleteEventDialog({ openDeleteEventDialog, eventName, handleDeleteEventDialogClose }) {
+  return (
+    <Dialog
+      open={openDeleteEventDialog}
+      onClose={handleDeleteEventDialogClose}
+    >
+      <DialogTitle>
+        Delete This Event
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {`Are you sure you want to delete the event "${eventName}"? This action is permanent and cannot be undone.`}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={handleDeleteEventDialogClose}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={handleDeleteEventDialogClose}
+        >
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default DeleteEventDialog;
