6c2161c4f99f0c95425c165d8466ea288b1d3b17
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 20:01:33 -0600

    Handle GET req to /user/recipes, sends bak all recipes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -5,7 +5,18 @@ import { Recipes } from '../db/index.js';
 // Create a new instance of express router
 const recipes = express.Router();
 
-// For POST requests to /users/recipes 
+// For GET requests to /user/recipes
+recipes.get('/', (req, res) => {
+  // Get al the recipes from the database
+  Recipes.find({})
+    .then((recipes) => {
+      res.status(200).send(recipes);
+    }).catch((err) => {
+      console.error('Error GETting recipes from the database: ', err);
+      res.sendStatus(500);
+    });
+})
+// For POST requests to /user/recipes 
 recipes.post('/', (req, res) => {
   // Grab the config from the req body
   const { recipe } = req.body;
