df60f2a95f8464e0acc0405437919706318ebf40
evan.loria4 <evan.loria4@gmail.com>
2025-01-08 19:47:49 -0600

    Initialize RecipesGrid component to render recipes

---
+++ client/components/recipes/RecipesGrid.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Grid from '@mui/material/Grid2';
+import {
+
+} from '@mui/material'
+
+function RecipesGrid() {
+  return (
+    <Grid container spacing={1}>
+    </Grid>
+  )
+}
