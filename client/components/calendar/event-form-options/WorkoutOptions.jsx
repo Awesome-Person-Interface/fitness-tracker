2ae2ae19c89fc576ac7e0fe5588645b8a63ddb87
tymey <tyler.meyer.a@gmail.com>
2025-01-10 16:48:33 -0600

    Update: WorkoutOptions fetches user's workout on mount

--- client/components/calendar/event-form-options/WorkoutOptions.jsx
+++ client/components/calendar/event-form-options/WorkoutOptions.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function WorkoutOptions() {
+  const [workouts, setWorkouts] = useState([]);
+
+  const getRoutines = () => {
+    axios.get('/user/routines/all')
+    .then(({ data }) => {
+      setWorkouts(data);
+    })
+    .catch((err) => {
+      console.log('Failed to getWorkouts:', err);
+    });
+  };
+
+  useEffect(() => {
+    getRoutines();
+  }, []);
+
+  console.log(workouts);
+
   return (
     <div>Workout Options</div>
   );
