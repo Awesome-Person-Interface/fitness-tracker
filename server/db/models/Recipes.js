37a9affe8edfe596b2a42c33d177334b3cd14242
evan.loria4 <evan.loria4@gmail.com>
2025-01-10 14:15:22 -0600

    Update recipes schemas with Nutrition subdocuments

--- server/db/models/Recipes.js
+++ server/db/models/Recipes.js
@@ -7,6 +7,21 @@ const IngredientSchema = new Schema({
   amount: {type: String },
   unit: { type: String },
 });
+// Initialize nutritient schema to hold nutrition name and unit
+const NutrientSchema = new Schema({
+  amount: { type: Number, required: true, },
+  unit: { type: String, required: true },
+})
+// Initialize nutrition schema to go on the RecipeSchema 
+const NutritionSchema = new Schema({
+  calories: NutrientSchema,
+  totalFat: NutrientSchema,
+  unSaturated: NutrientSchema,
+  sodium: NutrientSchema,
+  carbs: NutrientSchema,
+  sugar: NutrientSchema,
+  protein: NutrientSchema,
+});
 // Initialize the recipes schema
   // Use the iIngredientSchema for the ingredients field in recipes
   // Declaring an array of objects that follow the IngredientSchema
@@ -16,7 +31,8 @@ const RecipeSchema = new Schema({
   time: { type: String, },
   ingredients: [IngredientSchema],
   image: { type: String, default: null},
-  notes: { type: String }
+  notes: { type: String },
+  nutrition: NutritionSchema,
 });
 // Create the recipes model using RecipeSchema
 const Recipes = mongoose.model('Recipe', RecipeSchema);
