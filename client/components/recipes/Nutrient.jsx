bd284bee2bf072ba34a3b90b476a389e7b9c4b8b
evan.loria4 <evan.loria4@gmail.com>
2025-01-10 17:10:50 -0600

    Initialize Nutrient component in new file

---
+++ client/components/recipes/Nutrient.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import {
+  ListItem,
+  Typography,
+  Divider,
+} from '@mui/material';
+import Grid from '@mui/material/Grid2';
+
+function Nutrient({ nutrient }) {
+  return (
+    <Grid>
+      
+    </Grid>
+  )
+}
