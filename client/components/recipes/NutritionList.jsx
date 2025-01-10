bd165b453a8ca6d3d088039539d958e6b73e0a41
evan.loria4 <evan.loria4@gmail.com>
2025-01-10 17:06:22 -0600

    Initialize NutritionList component in new file

---
+++ client/components/recipes/NutritionList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+import {
+
+} from '@mui/material';
+
+function NutritionList() {
+  return (
+    <>
+    </>
+  )
+};
+
+export default NutritionList;
