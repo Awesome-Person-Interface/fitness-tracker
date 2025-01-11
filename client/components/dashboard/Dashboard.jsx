94fa6945b0195ac9555def8a5c361ca6d1f89718
stephen <jimisax@gmail.com>
2025-01-11 01:53:42 -0600

    MOVE Dashboard.jsx components for asthetics. Empty form info after submission

--- client/components/dashboard/Dashboard.jsx
+++ client/components/dashboard/Dashboard.jsx
@@ -79,6 +79,8 @@ const updateGoals = () => {
     // if after the patch, the weight (& l8r goalWeight) is(are) populated in the Schema,
     // tie each value to its corresponding db state
     getUserGoals();
+    setWeight('');
+    setGoalWeight('');
   }).catch((err) =>
   console.log('Could not patch goals', err)
 );
@@ -104,6 +106,41 @@ const updateGoals = () => {
           </Box>
         </Container>
       </Grid>
+
+      <div id="dash_container" style={{display: "flex", flexDirection: "column", paddingTop: "35px", justifyContent: "center"}}>
+        <div id="dash_workouts" style={{paddingRight:"20px"}}>
+          <DashboardWorkouts workouts={props.user.workouts}/>
+        </div>
+        <div id="dash_meals">
+          <DashboardMeals nutrition={props.user.nutrition}/>
+        </div>
+      </div>
+      <Grid size={5} justifyContent="space-between" alignItems="flex">
+        <FormControl variant="outlined" size="small">
+          <label htmlFor="current-weight">Current Weight:
+            <Input type="text"
+             name="current-weight"
+             value={weight}
+             onChange={() => setWeight(event.target.value)}
+             />
+          </label>
+          </FormControl>
+            
+          <FormControl>
+          <label type="text" htmlFor="goal-weight">Goal Weight:
+            <Input type="text"
+             name="goal-weight"
+             value={goalWeight}
+             onChange={() => setGoalWeight(event.target.value)}
+             />
+          </label>
+          <Button variant="contained"
+          type="submit"
+           name="submit-goal"
+           onClick={updateGoals}
+           >Set User Info</Button>
+        </FormControl>
+      </Grid>
       <Grid>
         <Container>
           <Box>
@@ -125,54 +162,12 @@ const updateGoals = () => {
     justifyContent="space-between"
     >
 
-      {/* <Container> */}
-
-      <Grid size={5} justifyContent="space-between" alignItems="flex">
-        <FormControl variant="outlined" size="small">
-          <label htmlFor="current-weight">Current Weight:
-            <Input type="text"
-             name="current-weight"
-             value={weight}
-             onChange={() => setWeight(event.target.value)}
-             />
-          </label>
-          {/* <Button variant="contained"
-           type="submit"
-            name="submit-weight"
-            onClick={updateGoals}
-            >Set Current Weight</Button> */}
-          </FormControl>
-            
-          <FormControl>
-          <label type="text" htmlFor="goal-weight">Goal Weight:
-            <Input type="text"
-             name="goal-weight"
-             value={goalWeight}
-             onChange={() => setGoalWeight(event.target.value)}
-             />
-          </label>
-          <Button variant="contained"
-          type="submit"
-           name="submit-goal"
-           onClick={updateGoals}
-           >Set User Info</Button>
-        </FormControl>
-        {/* </Container> */}
-      </Grid>
-      <Grid size={5}
+      {/* <Grid size={5}
       alignItems="flex"
       justifyContent="space-between"
       >
-      </Grid>
+      </Grid> */}
       <Divider/>
-      <div id="dash_container" style={{display: "flex", flexDirection: "column", paddingTop: "35px", justifyContent: "center"}}>
-        <div id="dash_workouts" style={{paddingRight:"20px"}}>
-          <DashboardWorkouts workouts={props.user.workouts}/>
-        </div>
-        <div id="dash_meals">
-          <DashboardMeals nutrition={props.user.nutrition}/>
-        </div>
-      </div>
     </Grid>
     </Grid>
     </div>
